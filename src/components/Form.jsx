5f63dfe086bce21bab499220f55ad64e85a94b1a
Mario Munteanu <mario.munteanu@cirvianum.cat>
2024-04-17 15:40:57 +0200

    Form finished

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 
-const [firstName, setFirstName] = useState("");
-
 function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setWelcomeMessage(`Benvingut, ${firstName} ${lastName}`);
+  };
+
   return (
     <div>
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-md mx-auto">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-md mx-auto"
+        onSubmit={handleSubmit}
+      >
         <h1 className="text-2xl font-bold mb-4">Informació Estudiant</h1>
         <label className="block text-gray-700 text-sm font-bold mb-2">
           Nom:
@@ -25,6 +35,7 @@ function Form() {
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="lname"
+          onBlur={(e) => setLastName(e.target.value)}
         />
         <br />
         <br />
@@ -33,6 +44,9 @@ function Form() {
           type="submit"
           value="Submit"
         />
+        <label className="block w-full text-4xl mb-4 p-2" id="studentMsg">
+          {welcomeMessage}
+        </label>
       </form>
     </div>
   );
